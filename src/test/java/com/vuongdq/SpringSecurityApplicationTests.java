0746b33b8239a2fde3cc0c25d1df7ed675ad1c45
vuongdq <quocvuong_it@yahoo.com>
2022-07-13 01:46:35 +0700

    addnew

---
+++ src/test/java/com/vuongdq/SpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vuongdq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
